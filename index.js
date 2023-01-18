35c204171b900cf77b19fe333adf0086c9a52f3f
Orlando Castillo <omaddoc@gmail.com>
2023-01-18 09:39:15 -0500

    sprint completed

--- index.js
+++ index.js
@@ -327,8 +327,13 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/) {
+function artistInfo(/*Your Code Here*/ arr, name) {
   /*Your Code Here*/
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name === name) {
+      return arr[i].bio;
+    }
+  }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -341,8 +346,15 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/) {
+function artistByCountry(/*Your Code Here*/ arr, nationality) {
   /*Your Code Here*/
+  let selectedArtists = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].nationality === nationality) {
+      selectedArtists.push(arr[i].name);
+    }
+  }
+  return selectedArtists;
 }
 
 /* ***** END OF TASKS ***** */
